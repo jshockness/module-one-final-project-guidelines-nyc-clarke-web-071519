f622196ec32bc47f0a4fa1180b4d3dbfec3f35da
Jason Shockness <jshock12@gmail.com>
2019-07-29 17:07:24 -0400

    initial app setup

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ gem "sinatra-activerecord"
 gem "sqlite3"
 gem "pry"
 gem "require_all"
+gem 'colorize'
+gem 'tty-prompt'
+gem 'tty-font'
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,13 @@
 require_relative '../config/environment'
+require 'tty-font'
+require 'tty-prompt'
+
+
+font = TTY::Font.new(:doom)
+prompt = TTY::Prompt.new
+
+puts font.write("Welcome to Mood Music Maker").colorize(:red)
+prompt.select("What mood are you in today??", %w(Happy Sad Workout Chill Throwbacker))
 
 
 
-puts "HELLO WORLD"
