a9bf6065c47770014dd8dd6c6851824fe85843e3
Jason Shockness <jshock12@gmail.com>
2019-07-31 22:41:12 -0400

    jasonshockness

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ end
 def add_favorite(playlist)
     f = Favorite.create(playlist_id: playlist.id, user_id: self.id)
   
-  end
+end
 
-  def delete_favorite(playlist)
+def delete_favorite(playlist)
     fave_id = Favorite.where(playlist_id: playlist.id, user_id: self.id).first.id
     Favorite.destroy(fave_id)
   
-  end
+end
 
 # quick git test
