6af5f0bb9a87b31e634796b9af0d10b58e72fc88
Alyssa Farina <alyssafarina695@gmail.com>
2019-08-01 10:48:30 -0400

    minor updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,13 @@ end
 
 def add_favorite(playlist)
     f = Favorite.create(playlist_id: playlist.id, user_id: self.id)
+    
   end
 
   def delete_favorite(playlist)
-    fave_id = Favorite.where(playlist_id: playlist.id, user_id: self.id).first.id
+    fave_id = Favorite.where(playlist_id: recipe.id, user_id: self.id).first.id
     Favorite.destroy(fave_id)
-  
   end
+
 
 # quick git test
